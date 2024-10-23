3775971ef9dd9ee4f795c19b7336b5e82343efdb
derrik-gratz <derrik.gratz@gmail.com>
2024-10-23 10:42:56 -0700

    remainder barcoded for scale

--- figure_scripts/cell_recovery.R
+++ figure_scripts/cell_recovery.R
@@ -2,37 +2,79 @@ library(tidyverse)  ## General R logic
 library(here)       ## Easier specification of file locations
 library(yaml)       ## parses config yaml
 library(Seurat)
+library(readxl)
+library(data.table)
+library(ggbreak)
 
 main <- function() {
-  fig_objs <- readRDS(here('rds/02-filtered_objs.rds'))
-  metadata <- read.csv(here('config/metadata.csv'))
+  fig_objs <- readRDS(here('rds/02-objs_post_cell_filtering.rds'))
+  kit_order <- read.table(here('config/kit_order.txt'))$V1
+  metadata <- read.csv(here('config/metadata.csv')) %>%
+    mutate(Kit = factor(Kit, levels = kit_order))
+  
   cell_recovery_figures <- list()
   
+  pipeline_data <- read_xlsx(here('data/pipeline_summary_statistics/downsampled_data.xlsx'), skip = 1) %>%
+    filter(!METRICS %in% c('Last updated')) %>%
+    # mutate(across(-c('METRICS'), as.numeric)) %>%
+    column_to_rownames('METRICS') %>%
+    t() %>% 
+    as.data.frame() %>%
+    rownames_to_column('Sample') %>%
+    as.data.table() %>%
+    select('Sample', '# cells loaded', '#Cells') %>%
+    melt(id.vars='Sample') %>%
+    mutate(value = as.numeric(value)) %>%
+    mutate(variable = case_when(
+      variable == '# cells loaded' & grepl('Scale', Sample) ~ '# cells barcoded',
+      .default = variable
+    ))
+  colnames(pipeline_data) <- c('Sample', 'class', 'Freq')
+  scale_used_cells <- 125000
+  pipeline_data <- rbind(pipeline_data,
+    data.frame(Sample = metadata$Sample[metadata$Kit=='Scale'], 
+             class = '# cells loaded', 
+             Freq = scale_used_cells
+  ))
+    
   doublets <- lapply(fig_objs, function(x) {
-    x$scDblFinder.class %>% 
-      table() %>%
-      as.data.frame()
-  }) %>% 
-    data.table::rbindlist(idcol = 'Sample') %>%
-    dplyr::rename('class' = '.') %>%
-    group_by(Sample)
+    x@misc$filtering_receipts$after_doublet_filtering[[2]]
+  }) %>% stack()
+  colnames(doublets) <- c('Freq', 'Sample')
+  doublets$class <- 'after_doublet_filtering'
   
   bad_cells <- lapply(fig_objs, function(x) {
-    x@misc$filtering_receipts$original_capture[[2]] - x@misc$filtering_receipts$after_qc_filtering[[2]]
+    x@misc$filtering_receipts$after_qc_filtering[[2]]
   }) %>% stack()
   colnames(bad_cells) <- c('Freq', 'Sample')
-  bad_cells$class <- 'low_quality_cell'
+  bad_cells$class <- 'after_qc_filtering'
   
-  plotdata <- rbind(doublets, bad_cells) %>%
+  plotdata <- do.call(rbind, list(pipeline_data, doublets, bad_cells)) %>%
     merge(metadata, by='Sample') %>%
     arrange(Sample, class) %>%
-    mutate(class = factor(class, levels = c('low_quality_cell', 'doublet', 'singlet')))
+    mutate(class = factor(class, levels = c('# cells barcoded', '# cells loaded', '#Cells', 'after_qc_filtering', 'after_doublet_filtering'))) %>%
+    group_by(Sample) %>%
+    arrange(Sample, class) %>%
+    mutate(Freq=as.numeric(Freq),
+             next_remaining = lead(Freq, default = 0),  # Next step's remaining count (or 0 for the last step)
+             difference = Freq - next_remaining         # Difference from the next entry
+           )
   
-  
-  ggplot(plotdata, aes(x=paste0(Individual, Replicate), y=Freq, fill=class)) +
+  ggplot(plotdata, aes(x=paste0(Individual, Replicate), y=difference, fill=class)) +
     geom_col(position='stack') +
     facet_wrap(~Kit, scales = 'free_x', nrow=1) +
-    scale_fill_manual(values = c('darkred', 'goldenrod', 'darkgreen'), labels = c('Low quality cell', 'Multiplet', 'Singlet')) +
+    ggbreak::scale_y_break(c(130000, 200000), ticklabels = c(200000, 230000))+
+    scale_fill_manual(values = c('darkgrey', '#7c1d6f', '#Cf597e', '#e9e29c', '#39B185'), labels = c('Remainder barcoded', 'Unrecovered cells', 'Low quality cells', 'Multiplets', 'High quality singlet')) +
+    labs(x='Sample', y='Number of cells', fill='Classification') ->
+    cell_recovery_figures[['cell_recovery_counts_break']] 
+  ggsave(filename = 'cell_recovery_counts_break.png', path = here('figures'),
+         device = 'png', 
+         height = unit(7, 'in'), width = unit(9, 'in'))
+  
+  ggplot(plotdata, aes(x=paste0(Individual, Replicate), y=difference, fill=class)) +
+    geom_col(position='stack') +
+    facet_wrap(~Kit, scales = 'free_x', nrow=1) +
+    scale_fill_manual(values = c('darkgrey', '#7c1d6f', '#Cf597e', '#e9e29c', '#39B185'), labels = c('Remainder barcoded', 'Unrecovered cells', 'Low quality cells', 'Multiplets', 'High quality singlet')) +
     labs(x='Sample', y='Number of cells', fill='Classification') ->
     cell_recovery_figures[['cell_recovery_counts']] 
   ggsave(filename = 'cell_recovery_counts.png', path = here('figures'),
@@ -41,14 +83,28 @@ main <- function() {
   
   plotdata %>% 
     group_by(Sample) %>%
-    mutate(Freq = Freq / sum (Freq)) %>%
-    ggplot(aes(x=paste0(Individual, Replicate), y=Freq, fill=class)) +
+    mutate(difference = difference / sum (difference)) %>%
+    ggplot(aes(x=paste0(Individual, Replicate), y=difference, fill=class)) +
     geom_col(position='stack') +
     facet_wrap(~Kit, scales = 'free_x', nrow=1) +
-    scale_fill_manual(values = c('darkred', 'goldenrod', 'darkgreen'), labels = c('Low quality cell', 'Multiplet', 'Singlet')) +
+    scale_fill_manual(values = c('darkgrey', '#7c1d6f', '#Cf597e', '#e9e29c', '#39B185'), labels = c('Remainder barcoded', 'Unrecovered cells', 'Low quality cells', 'Multiplets', 'High quality singlet')) +
     labs(x='Sample', y='Portion of capture', fill='Classification') ->
     cell_recovery_figures[['cell_recovery_portions']] 
   ggsave(filename = 'cell_recovery_portions.png', path = here('figures'),
+         device = 'png', 
+         height = unit(7, 'in'), width = unit(9, 'in')) 
+  
+  plotdata %>% 
+    group_by(Sample) %>%
+    filter(class != '# cells barcoded') %>%
+    mutate(difference = difference / sum (difference)) %>%
+    ggplot(aes(x=paste0(Individual, Replicate), y=difference, fill=class)) +
+    geom_col(position='stack') +
+    facet_wrap(~Kit, scales = 'free_x', nrow=1) +
+    scale_fill_manual(values = c('#7c1d6f', '#Cf597e', '#e9e29c', '#39B185'), labels = c('Unrecovered cells', 'Low quality cells', 'Multiplets', 'High quality singlet')) +
+    labs(x='Sample', y='Portion of capture', fill='Classification') ->
+    cell_recovery_figures[['cell_recovery_portions_nocellbarcoded']] 
+  ggsave(filename = 'cell_recovery_portions_noremainderbarcoded.png', path = here('figures'),
          device = 'png', 
          height = unit(7, 'in'), width = unit(9, 'in')) 
   
