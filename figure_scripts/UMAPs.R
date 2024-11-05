eb6a45618e1336f324d38afe82e11693f0f3bbc2
derrik-gratz <derrik.gratz@gmail.com>
2024-11-05 10:16:26 -0800

    added bcell subtypes

--- figure_scripts/UMAPs.R
+++ figure_scripts/UMAPs.R
@@ -13,35 +13,78 @@ main <- function() {
     
   umap_figures <- list()
   
-  cell_colors <- list(
-    'T' = "#E41A1C",
-    'B' = "#377EB8",
-    'Dendritic' = "#4DAF4A",
-    'Monocyte' = "#984EA3",
-    'NK' = "#FF7F00",
-    'Platelet' = "#A65628",
-    'Unknown' = "darkgrey"
-  )
+  cell_colors <- list(coarse=c(     
+    "T" = "blue",
+    "B" = "red",
+    "Monocyte" = "purple",
+    "NK" = "#4DAF4A",
+    "Megakaryocyte" = "#A65628",
+    "Dendritic" = "orange",
+    "pDC" = "gold",
+    'Unknown' = 'grey'
+    ), fine = c(
+    'T' = "blue",
+    'CD8+ T' = "#377EB8",
+    'CD4+ T' = "#80B1D3",
+    'B naive' = "#FB8072",
+    'B memory' = "#E41A1C",
+    'Monocyte' = "purple",
+    'Classical monocyte' = "#BC80BD",
+    'Non-classical monocyte' = "#984EA3",
+    'NK' = "#4DAF4A",
+    'Megakaryocyte' = "#A65628",
+    'Dendritic' =  "orange", 
+    'pDC' = "gold", 
+    'Unknown' = 'grey'
+    )
+  ) 
+  my_dimplot <- function(obj, reduction='umap', dims=c(1,2), group.by, colors = cell_colors$fine){
+    dimdata <- obj@reductions[[reduction]]@cell.embeddings[,dims]
+    dimdata[,1] <- scale(dimdata[,1])
+    dimdata[,2] <- scale(dimdata[,2])
+    plotdata <- merge(dimdata, obj@meta.data, by='row.names')
+    plotdata[[group.by]] <- factor(plotdata[[group.by]], levels=names(colors))
+    ggplot(plotdata, aes(x=.data[[paste0(reduction, '_', dims[1])]],
+                         y=.data[[paste0(reduction, '_', dims[2])]],
+                         color=.data[[group.by]])) +
+      geom_point(size=0.1, show.legend = TRUE) +
+      scale_color_manual(values = colors, breaks = names(colors), drop=FALSE) +
+      labs(x=paste0(reduction, '_', dims[1]), y=paste0(reduction, '_', dims[2]), color='Cell type') +
+      theme_classic() +
+      theme(axis.text = element_blank(), 
+            axis.title = element_blank(),
+            axis.ticks = element_blank()) +
+      guides(colour = guide_legend(override.aes = list(size=3))) +
+      ggtitle(gsub('.+_(.+)$', '\\1', obj@project.name))
+      
+  }
+  
+  extras <- c("#CCEBC5",  "#FB8072",  "#FCCDE5", "#4DAF4A")
   for (kit in unique(metadata$Kit)) {
-    # for (sample in metadata$Sample[metadata$Kit==kit]) {
     samples <- metadata$Sample[metadata$Kit==kit]
     ps <- lapply(samples, function(sample){
-      ind <- metadata$Individual[metadata$Sample==sample]
-      rep <- metadata$Replicate[metadata$Sample==sample]
-      DimPlot(fig_objs[[sample]], group.by = 'cell_labels.coarse', label=FALSE, order=rev(names(cell_colors))) + 
-        scale_color_manual(values = cell_colors, drop=FALSE, show.legend=TRUE) +
-        ggtitle(paste0(ind, rep)) + 
-        theme(axis.text = element_blank(), 
-              axis.title = element_blank(),
-              axis.ticks = element_blank())
+      my_dimplot(fig_objs[[sample]], group.by = 'cell_labels.fine')
     })
-    umap_figures[[kit]] <- ps[[1]] + ps[[2]] + ps[[3]] + ps[[4]] + 
+    umap_figures[[paste0(kit, '_fine')]] <- ps[[1]] + ps[[2]] + ps[[3]] + ps[[4]] + 
+      plot_layout(guides='collect')+ 
+      plot_annotation(title = kit,
+                      theme = theme(plot.title = element_text(size = 24, hjust = 0.5, face='bold')))
+    ggsave(plot = umap_figures[[paste0(kit, '_fine')]],
+           path= here('figures/UMAPs'), filename=paste0(kit, '_fine_labels.png'), device = 'png', 
+           width = unit(10, 'in'), height = unit(8, 'in'), )
+  }
+  for (kit in unique(metadata$Kit)) {
+    samples <- metadata$Sample[metadata$Kit==kit]
+    ps <- lapply(samples, function(sample){
+      my_dimplot(fig_objs[[sample]], group.by = 'cell_labels.coarse', colors = cell_colors$coarse)
+    })
+    umap_figures[[paste0(kit, '_coarse')]] <- ps[[1]] + ps[[2]] + ps[[3]] + ps[[4]] + 
       plot_layout(ncol = 2, guides='collect') + 
       plot_annotation(title = kit, 
                       theme = theme(plot.title = element_text(size = 24, hjust = 0.5, face='bold')))
-    ggsave(plot = umap_figures[[kit]],
+    ggsave(plot = umap_figures[[paste0(kit, '_coarse')]],
            path= here('figures/UMAPs'), filename=paste0(kit, '_coarse_labels.png'), device = 'png', 
-           width = unit(9, 'in'), height = unit(8, 'in'), )
+           width = unit(10, 'in'), height = unit(8, 'in'), )
   }
   return(umap_figures)
 }
