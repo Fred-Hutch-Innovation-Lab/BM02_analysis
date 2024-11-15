687ed107469b3fd549f86f307ae2c98afd58ade3
derrik-gratz <derrik.gratz@gmail.com>
2024-11-15 10:08:48 -0800

    add fluent v5 & bd

--- config/color_palette.R
+++ config/color_palette.R
@@ -3,13 +3,14 @@
 
 create_color_palette <- function() {
   palette <- c(
-    other =     "#E69F00", #rgb(230, 159,   0, maxColorValue = 255),  # Orange 
+    Fluent_v5 =     "#E69F00", #rgb(230, 159,   0, maxColorValue = 255),  # Orange 
     NextGEM3P = "#56B4E9", #rgb(86,  180, 233, maxColorValue = 255),  # Sky blue
     GEMX3P =    "#0072B2", #rgb(0,   114, 178, maxColorValue = 255),  # Blue
-    Fluent =    "#F0E442", #rgb(240, 228,  66, maxColorValue = 255),  # Yellow
+    Fluent_v4 =    "#F0E442", #rgb(240, 228,  66, maxColorValue = 255),  # Yellow
     Parse_V3 =  "#009E73", #rgb(0,   158, 115, maxColorValue = 255),  # Bluish green
     Scale =     "#CC79A7", #rgb(204, 121, 167, maxColorValue = 255),  # pink
-    Flex =      "#D55E00"  #rgb(213,  94,   0, maxColorValue = 255)   # Vermillion
+    Flex =      "#D55E00",  #rgb(213,  94,   0, maxColorValue = 255)   # Vermillion
+    BD = 'black',
   )
   return(palette)
 }
--- config/generate_metadata.R
+++ config/generate_metadata.R
@@ -6,11 +6,13 @@ rbindlist <- data.table::rbindlist
 config <- read_yaml(here('config/config.yaml'))
 data_dirs <- list(
   "B2-FLEX_10k"="B2-FLEX_10k/full_data",
-  "B2-Fluent_25k"="B2-Fluent_25k/OutPut_25K",
+  "B2-FluentV4_25k"="B2-FluentV4_25k/OutPut_25K",
+  "B2-FluentV5_25k"="B2-FluentV5_25k",
   "B2-GEMX3P_25k"="B2-GEMX3P_25k",
   "B2-NextGEM3P_25k"="B2-NextGEM3P_25k",
-  "B2-Parse_25k"="B2-Parse_25k/OutPut_25K_V.1.1/COMBINED/",
-  "B2-Scale_25k"="B2-Scale_25k"
+  "B2-Parse_25k"="B2-Parse_25k/OutPut_25K_V.1.2.1/COMBINED/",
+  "B2-Scale_25k"="B2-Scale_25k",
+  "B2-BD_WTA_25k"="B2-BD_WTA_25k"
 )
 samplesheets <- lapply(names(data_dirs), function(x) {
   read.table(here(file.path('config', paste0(x, '_samplesheet.txt'))), header = TRUE)
