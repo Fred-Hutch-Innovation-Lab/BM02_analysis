42cfab41ca457612e5a6ace7130bfbf9d2837c8d
derrik-gratz <derrik.gratz@gmail.com>
2024-11-27 16:55:43 -0800

    update metadata with new fluent

--- config/color_palette.R
+++ config/color_palette.R
@@ -3,14 +3,14 @@
 
 create_color_palette <- function() {
   palette <- c(
-    Fluent_v5 =     "#E69F00", #rgb(230, 159,   0, maxColorValue = 255),  # Orange 
-    NextGEM3P = "#56B4E9", #rgb(86,  180, 233, maxColorValue = 255),  # Sky blue
-    GEMX3P =    "#0072B2", #rgb(0,   114, 178, maxColorValue = 255),  # Blue
-    Fluent_v4 =    "#F0E442", #rgb(240, 228,  66, maxColorValue = 255),  # Yellow
+    NextGEM3P = "#E69F00", #rgb(230, 159,   0, maxColorValue = 255),  # Orange 
+    Fluent_v5 = "#56B4E9", #rgb(86,  180, 233, maxColorValue = 255),  # Sky blue
+    Fluent_v4 = "#0072B2", #rgb(0,   114, 178, maxColorValue = 255),  # Blue
+    GEMX3P =    "#F0E442", #rgb(240, 228,  66, maxColorValue = 255),  # Yellow
     Parse_V3 =  "#009E73", #rgb(0,   158, 115, maxColorValue = 255),  # Bluish green
     Scale =     "#CC79A7", #rgb(204, 121, 167, maxColorValue = 255),  # pink
     Flex =      "#D55E00",  #rgb(213,  94,   0, maxColorValue = 255)   # Vermillion
-    BD = 'black',
+    BD = 'black'
   )
   return(palette)
 }
--- config/generate_metadata.R
+++ config/generate_metadata.R
@@ -11,8 +11,8 @@ data_dirs <- list(
   "B2-GEMX3P_25k"="B2-GEMX3P_25k",
   "B2-NextGEM3P_25k"="B2-NextGEM3P_25k",
   "B2-Parse_25k"="B2-Parse_25k/OutPut_25K_V.1.2.1/COMBINED/",
-  "B2-Scale_25k"="B2-Scale_25k",
-  "B2-BD_WTA_25k"="B2-BD_WTA_25k"
+  "B2-Scale_25k"="B2-Scale_25k"#,
+  # "B2-BD_WTA_25k"="B2-BD_WTA_25k"
 )
 samplesheets <- lapply(names(data_dirs), function(x) {
   read.table(here(file.path('config', paste0(x, '_samplesheet.txt'))), header = TRUE)
