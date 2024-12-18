e5661bd3d932dd7f6a775bc3e9ae67a43b264f2e
derrik-gratz <derrik.gratz@gmail.com>
2024-12-18 11:49:37 -0800

    scale sample swap

--- config/color_palette.R
+++ config/color_palette.R
@@ -8,6 +8,7 @@ create_color_palette <- function() {
       Fluent_V = "#56B4E9", #rgb(86,  180, 233, maxColorValue = 255),  # Sky blue
       Fluent_v4 = "#0072B2", #rgb(0,   114, 178, maxColorValue = 255),  # Blue
       GEMX3P =    "#F0E442", #rgb(240, 228,  66, maxColorValue = 255),  # Yellow
+      Parse_v2 = 'darkgreen',
       Parse_v3 =  "#009E73", #rgb(0,   158, 115, maxColorValue = 255),  # Bluish green
       Scale =     "#CC79A7", #rgb(204, 121, 167, maxColorValue = 255),  # pink
       Flex =      "#D55E00",  #rgb(213,  94,   0, maxColorValue = 255)   # Vermillion
