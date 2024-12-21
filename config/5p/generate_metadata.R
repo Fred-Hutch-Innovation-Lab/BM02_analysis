030595815d8d9e8156a16b5ca5222e6da0ef8f3f
derrik-gratz <derrik.gratz@gmail.com>
2024-12-20 16:31:47 -0800

    fix paths with renaming 5p 3p

--- config/5p/generate_metadata.R
+++ config/5p/generate_metadata.R
@@ -3,17 +3,17 @@ library(here)       ## Easier specification of file locations
 library(yaml)
 rbindlist <- data.table::rbindlist
 
-config <- read_yaml(here('config/tcr/config.yaml'))
+config <- read_yaml(here('config/5p/config.yaml'))
 data_dirs <- list(
   "gemx_5p"="gemx_5p/downsampled",
   "nextgem_5p"="nextgem_5p/downsampled",
   "parse_v2"="parse_v2"
 )
 samplesheets <- lapply(names(data_dirs), function(x) {
-  read.table(here(file.path('config/tcr', paste0(x, '_samplesheet.txt'))), header = TRUE)
+  read.table(here(file.path('config/5p', paste0(x, '_samplesheet.txt'))), header = TRUE)
 })
 names(samplesheets) <- names(data_dirs)
 metadata <- data.table::rbindlist(samplesheets, fill=TRUE)
 
-write.table(metadata, here('config/tcr/metadata.csv'), quote=FALSE, row.names = FALSE, sep = ',')
+write.table(metadata, here('config/5p/metadata.csv'), quote=FALSE, row.names = FALSE, sep = ',')
             
