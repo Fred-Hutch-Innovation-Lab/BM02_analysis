1b91cfd3fe58ca1a5953132183cf4ce008e18598
derrik-gratz <derrik.gratz@gmail.com>
2024-12-20 16:25:21 -0800

    5p 3p split and wt tcr split in 5p

---
+++ figure_scripts/3p/run_all.R
@@ -0,0 +1,8 @@
+
+skip <- c('run_all.R')
+files <- dir(here('figure_scripts/3p'), pattern = '\\.R$')
+files <- files[!files %in% skip]
+for (file in files) {
+  tryCatch(source(here('figure_scripts', file)), 
+           error = function(e) {print(paste0('Error running file ', file))})
+}
