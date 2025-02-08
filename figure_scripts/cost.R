15412f7b22fddeb78c0fa4a1c4bd85676854843a
derrik-gratz <derrik.gratz@gmail.com>
2025-02-07 16:15:29 -0800

    extensive cost figures

--- figure_scripts/cost.R
+++ figure_scripts/cost.R
@@ -1,26 +1,19 @@
-library(tidyverse)  ## General R logic
-library(here)       ## Easier specification of file locations
-library(yaml)    
+# Setup ----
+source(here('figure_scripts/utils.R')) 
 library(readxl)
-library(data.table)
 
-source(here('config/color_palette.R'))
-source(here('config/kit_order.R'))
-kit_order <- append(kit_order_3p, 'Parse_v2', which(kit_order_3p == 'Parse_v3') - 1)
-metadata <- read.csv(here('config/3p/metadata.csv')) %>%
-  mutate(Kit = factor(Kit, levels = kit_order))
-
-
-cost_data_kit <- read_tsv(here('data/cost_v2.txt'))  %>%
+# Load data ----
+cost_data_kit <- read_csv(here('data/cost.txt'), sep='\t')  |>
+  mutate(cost = readr::parse_number(cost)) |>
     rowwise() %>%
     mutate(
       Extended = list(data.frame(
-        samples = 1:Samples#,
+        samples = 1:max_samples#,
       ))
     ) %>%
     unnest(cols = c(Extended)) |>
-  mutate(cost_per_sample = Cost / samples,
-         cells_per_sample = Cells / samples) |>
+  mutate(cost_per_sample = ifelse(all_in, cost / samples, cost / max_samples),
+         cells_per_sample = ifelse(all_in, max_cells / samples, max_cells / max_samples)) #|>
   select(-c(Samples, Cells, Cost)) |>
   as.data.table()
 # 
@@ -41,12 +34,14 @@ cost_data_kit <- read_tsv(here('data/cost_v2.txt'))  %>%
 
 
 cost_data_kit |>
-  ggplot(aes(x=samples, color=Kit, group=Kit)) +
-  geom_line(aes(y=cost_per_sample), alpha=0.8, lty='solid') +
+  filter(!kit_family %in% c('Parse_WT', 'Parse_TCR', 'Scale')) |>
+  ggplot(aes(x=samples, color=kit, group=kit)) +
+  geom_line(aes(y=cost_per_sample, linewidth=cells_per_sample), alpha=0.8, lty='solid') +
   # geom_step(aes(y=cells_per_sample), direction='hv', alpha=0.8, lty='dashed') +
   theme_bw() +
+  facet_wrap(~ kit_family, nrow=1, scales='fixed') +
   # scale_linewidth_continuous(range=c(1,4), breaks = c(10000, 20000, 30000)) +
-  scale_color_manual(values = unlist(color_palette$kits), labels = label_function) +
+  # scale_color_manual(values = unlist(color_palette$kits), labels = label_function) +
   labs(x='Samples', y='Cost per sample', color = 'Kit') #+ 
   # guides(color = guide_legend(override.aes = list(linewidth = 4)))
 
