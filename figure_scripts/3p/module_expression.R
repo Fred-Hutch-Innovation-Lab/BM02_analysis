71e0375b019b5d82c3175503429ee39a27fc0dad
derrik-gratz <derrik.gratz@gmail.com>
2025-02-18 09:57:35 -0800

    fix axis labels

--- figure_scripts/3p/module_expression.R
+++ figure_scripts/3p/module_expression.R
@@ -25,12 +25,12 @@ fig_objs <- readRDS(here('rds/3p/07_post_module_scoring.rds'))
 confusion_plot <- function(plotdata, kit){
   plotdata |>
     filter(Kit == kit) |>
-    ggplot(aes(x=cell_labels.fine, y=module, fill=value, label=round(value, 2))) +
+    ggplot(aes(x=cell_labels.fine, y=module, fill=value, label=ifelse(is.na(value), 'NA', value))) +
     geom_tile() +
     geom_text() +
     scale_fill_gradient(low = "white", high = "red", limits=c(0,1)) +
     theme(axis.text.x = element_text(angle=45, hjust=1)) +
-    labs(x='Cell label', y='Marker module', fill='Average\nscore')
+    labs(x='Cell label', y='Marker module', fill='Average\nmarker gene\nmodule score')
 }
 
 # Generate plotdata ----
@@ -47,19 +47,22 @@ plotdata <- lapply(fig_objs, function(x){
   select(-variable) |>
   mutate(Kit = factor(Kit, levels = kit_order_3p), 
          module=factor(module, levels=c(label_order$fine)),
-         cell_labels.fine = factor(cell_labels.fine, levels = label_order$fine)) 
+         cell_labels.fine = factor(cell_labels.fine, levels = label_order$fine),
+         value = round(value, 2)) |>
+  complete(Kit, module, cell_labels.fine, fill = list(value = NA)) |>
+  filter(cell_labels.fine != 'Unknown')
 
 # Plot ----
 plotdata |> filter(
     as.character(cell_labels.fine) == as.character(module)
   ) |> 
   filter(!module %in% c('B', 'T', 'Monocyte')) |> 
-  ggplot(aes(x=Kit, y=module, fill=value, label=round(value, 2))) +
+  ggplot(aes(x=Kit, y=module, fill=value, label=ifelse(is.na(value), 'NA', value))) +
   geom_tile() +
   geom_text() +
   scale_fill_gradient(low = "white", high = "red", limits=c(0,1)) +
   scale_x_discrete(labels = label_function) +
-  labs(x='Kit', y='Marker module', fill='Average\nscore') +
+  labs(x='Kit', y='Celltype', fill='Average\nmarker gene\nmodule score') +
   theme(axis.text.x = element_text(angle=45, hjust=1)) ->
 figures[['module_scores_kit_fine']] 
 
