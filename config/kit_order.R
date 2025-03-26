9b402480b9d0060f81388c3848b1d78f9e7a30c3
derrik-gratz <derrik.gratz@gmail.com>
2025-03-26 10:10:05 -0700

    extend label function

--- config/kit_order.R
+++ config/kit_order.R
@@ -24,22 +24,44 @@ kit_order_all <- c(
   'Parse_v3',
   'Scale'
 )
-label_function <- function(kits) {
-  lapply(kits, function(kit) {
-    case_when(
-      grepl('Flex', kit) ~ "Flex",
-      grepl('NextGEM3P', kit) ~ "NextGEM 3'",
-      grepl('GEMX3P', kit) ~ "GEMX 3'",
-      grepl('Fluent_v4', kit) ~ "Fluent v4",
-      grepl('Fluent_V', kit) ~ "Fluent V",
-      grepl('Parse_v3', kit) ~ "Parse v3",
-      grepl('Parse_v2', kit) ~ "Parse TCR",
-      grepl('Scale', kit) ~ "Scale",
-      grepl('NextGEM5P', kit) ~ "NextGEM 5'",
-      grepl('GEMX5P', kit) ~ "GEMX 5'",
-      .default = kit
-    )
-  }) |>
-    unlist() |>
-    unname()
+
+label_function <- function(mode = 'both', wrap = FALSE, width = 20) {
+  fun <- function(labels) {
+    lapply(labels, function(value){
+      x <- case_when(
+        grepl('Flex', value) ~ "Flex",
+        grepl('NextGEM3P', value) ~ "NextGEM 3'",
+        grepl('GEMX3P', value) ~ "GEMX 3'",
+        grepl('Fluent_v4', value) ~ "Fluent v4",
+        grepl('Fluent_V', value) ~ "Fluent V",
+        grepl('Parse_v3', value) ~ "Parse v3",
+        grepl('Parse_v2', value) ~ case_when(
+          mode == 'TCR' ~ "Parse v2 TCR",
+          mode == 'WT' ~ "Parse v2 WT",
+          mode == 'both' ~ "Parse v2 WT+TCR",
+          mode == 'clean' ~ 'Parse v2'
+        ),
+        grepl('Scale', value) ~ "Scale",
+        grepl('NextGEM5P', value) ~ case_when(
+          mode == 'TCR' ~ "NextGEM 5' TCR",
+          mode == 'WT' ~ "NextGEM 5' WT",
+          mode == 'both' ~ "NextGEM 5' WT+TCR",
+          mode == 'clean' ~ "NextGEM 5'"
+        ),
+        grepl('GEMX5P', value) ~ case_when(
+          mode == 'TCR' ~ "GEMX 5' TCR",
+          mode == 'WT' ~ "GEMX 5' WT",
+          mode == 'both' ~ "GEMX 5' WT+TCR",
+          mode == 'clean' ~ "GEMX 5'"
+        ),
+        .default = value
+      )
+      if (wrap) {
+        x <- str_wrap(x, width = width)
+      }
+      
+      return(x)
+    })
+  }
+  structure(fun, class = 'labeller')
 }
