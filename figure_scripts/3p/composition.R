0df49d5ebfe6e05ee366b571f4d13a5494b35aa7
derrik-gratz <derrik.gratz@gmail.com>
2025-05-05 16:44:31 -0700

    facet span text size

--- figure_scripts/3p/composition.R
+++ figure_scripts/3p/composition.R
@@ -5,7 +5,7 @@ source(here('figure_scripts/utils.R'))
 comp_table <- read.table(here('rds/3p/composition_table_coarse.txt'), sep = '\t', header = TRUE)
 
 # Plotting ----
-comp_table |> 
+plotdata <- comp_table |> 
   reshape2::melt(id.vars = c('Kit', 'Sample', 'Individual', 'Replicate')) |> #measure.vars=unique(coarse_mapping)) |> 
   mutate(variable = factor(variable, levels = c(
     'T', 'Monocyte', 'B', 'NK', 'Dendritic', 'Megakaryocyte', 'pDC', 
@@ -14,8 +14,9 @@ comp_table |>
   group_by(Sample) |> 
   mutate(percent = 100*value / sum(value),
          Kit = factor(Kit, levels=kit_order_3p)) |>
-  mutate(percent = ifelse(percent == 0, NA, percent)) |>
-  ggplot(aes(x = Kit, y = percent,
+  mutate(percent = ifelse(percent == 0, NA, percent)) 
+
+ggplot(plotdata, aes(x = Kit, y = percent,
              group = Kit, color = Kit, shape = paste0(Individual, Replicate))) + 
   geom_point() +
   labs(x = element_blank(), y = '% of sample',
@@ -26,12 +27,14 @@ comp_table |>
          # sep='\n')
 ) +
   scale_shape_manual(values = c(1:5))+ 
-  scale_color_manual(values=color_palette$kits, labels = label_function) +
+  scale_color_manual(values=color_palette$kits, labels = label_function(mode = 'WT')) +
   theme(axis.text.x = element_blank(), axis.ticks.x = element_blank()) +
-  facet_grid(~ factor(variable), scales='free_y') ->
+  facet_grid(~ factor(variable), scales='free_y') +
+  theme(strip.text.x = element_text(size = 10)) ->
   figures[['composition_boxplot']]
+figures[['composition_boxplot']]
 
-  my_plot_save(image = figures[['composition_boxplot']], 
-               path = here('figures/3p/composition_coarse_labels.svg'), 
-               width = 13, height = 4)
+my_plot_save(image = figures[['composition_boxplot']], 
+             path = here('figures/3p/composition_coarse_labels.svg'), 
+             width = 13, height = 4)
   
